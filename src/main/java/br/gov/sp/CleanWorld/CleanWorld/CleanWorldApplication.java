30a9d6473c7ae9c5555ac40867c88a023f7d709b
Thalyta Sales <thalyta.sales23@gmail.com>
2019-11-01 21:58:42 -0300

    aula0111

---
+++ src/main/java/br/gov/sp/CleanWorld/CleanWorld/CleanWorldApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.CleanWorld.CleanWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/CleanWorld/CleanWorld/CleanWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.CleanWorld.CleanWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4fa72d13b27ff8f386c2e69444c6dbcbc250eb60
Thalyta Sales <thalyta.sales23@gmail.com>
2019-11-01 22:28:02 -0300

    criação classe dao

--- src/main/java/model/TiposMaterial.java
+++ src/main/java/model/TiposMaterial.java
@@ -1,5 +1,29 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Table (name="CleanWorld")
+@Entity
+@Setter
+@Getter
+
+
 public class TiposMaterial {
+	
+	@Column (name="idTiposMaterial")
+	@GeneratedValue
+	@Id
+	private Long idTiposMaterial;
+	
+	
+	@Column (name="dsTiposMaterial")
+	private String dsTiposMaterial;
 
 }
